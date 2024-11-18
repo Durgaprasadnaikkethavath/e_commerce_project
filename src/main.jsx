a2c4b4d3f723b08f1c90372a158ac83b0f1853cb
Durgaprasadnaikkethavath <kethavathdurgaprasadnaik@gmail.com>
2024-11-18 16:47:41 +0530

    projects update

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 // import { StrictMode } from "react";
-import ReactDOM from "react-dom/canary";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
