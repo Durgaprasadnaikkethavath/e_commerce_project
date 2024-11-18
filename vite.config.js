99e7e0bfb5553370db1b3d5744f4e37dd130fb02
Durgaprasadnaikkethavath <kethavathdurgaprasadnaik@gmail.com>
2024-11-18 23:12:42 +0530

    new

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/e-commerce-project/",
+  base: "/e-commerce_project/",
   plugins: [react()],
 });
