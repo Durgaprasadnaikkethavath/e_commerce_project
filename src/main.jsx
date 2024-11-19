3797d86c3c5dbeba870d635f150c7b67ddf3bf8a
Durgaprasadnaikkethavath <kethavathdurgaprasadnaik@gmail.com>
2024-11-19 18:02:57 +0530

    new testing type

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,21 @@
 import { createRoot } from "react-dom/client";
-// import { StrictMode } from "react";
-import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+// import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
-import ShopContextProvider from "./Context/ShopContext.jsx";
+// import { BrowserRouter } from "react-router-dom";
+// import ShopContextProvider from "./Context/ShopContext.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <ShopContextProvider>
-      <App />
-    </ShopContextProvider>
-  </BrowserRouter>
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <BrowserRouter>
+//     <ShopContextProvider>
+//       <App />
+//     </ShopContextProvider>
+//   </BrowserRouter>
+// );
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
